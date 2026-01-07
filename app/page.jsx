f790df3c9d4a49bbf72328960b14dcb94e26b5e7
Shoishob007 <u1704049@student.cuet.ac.bd>
2026-01-07 15:35:12 +0600

    corrected routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import { redirect } from "next/navigation";
 import HomeContent from "@/app/(dashboard)/components/HomeContents";
 import ClientWrapper from "./ClientWrapper";
 
 export default function HomePage() {
+    redirect("/onboarding");
+
   return (
     <ClientWrapper>
       <HomeContent />
