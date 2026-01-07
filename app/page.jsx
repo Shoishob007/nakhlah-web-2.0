426cd55ee1c0a5e843f37f0d8f7e2dcbcb6b0812
Shoishob007 <u1704049@student.cuet.ac.bd>
2026-01-07 15:41:50 +0600

    reverted the routing

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import HomeContent from "@/app/(dashboard)/components/HomeContents";
 import ClientWrapper from "./ClientWrapper";
 
 export default function HomePage() {
-    redirect("/onboarding");
 
   return (
     <ClientWrapper>
